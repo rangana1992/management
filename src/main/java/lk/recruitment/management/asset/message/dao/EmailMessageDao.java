d2c980f0697f4b98f6c2d6fcc696397973f13028
rangana1992 <abesundara.ssr@gmail.com>
2020-09-06 20:36:48 +0530

    Email message service was removed

--- src/main/java/lk/recruitment/management/asset/message/dao/EmailMessageDao.java
+++
@@ -1,9 +0,0 @@
-package lk.recruitment.management.asset.message.dao;
-
-import lk.recruitment.management.asset.message.entity.EmailMessage;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmailMessageDao extends JpaRepository<EmailMessage, Integer > {
-}
