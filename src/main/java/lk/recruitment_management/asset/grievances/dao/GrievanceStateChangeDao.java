a3862e6bbf4c30b6c36a23de4c1bbc521dcea663
WB Prasad <airportprasad@gmail.com>
2021-05-06 22:01:38 +0530

    Add Grievances

--- src/main/java/lk/recruitment_management/asset/grievances/dao/GrievanceStateChangeDao.java
+++
@@ -1,9 +0,0 @@
-package lk.recruitment_management.asset.grievances.dao;
-
-import lk.avsec_welfare.asset.grievances.entity.GrievanceStateChange;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GrievanceStateChangeDao extends JpaRepository< GrievanceStateChange,Integer> {
-}
